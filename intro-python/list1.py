fdaf2030a9501dbffd719ee64d6dc4af1242ace6
Student User <student@cs.pub.ro>
2014-06-25 11:17:51 +0300

    list 1 resolved

--- intro-python/list1.py
+++ intro-python/list1.py
@@ -36,8 +36,16 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
+    list1, list2, s = [], [], []
+    for i in words:
+        if i[0] == 'x':
+            list1.append(i)
+        else:
+            list2.append(i)
+    list1.sort()
+    list2.sort()
+    s = list1+ list2
+    return s
 
 
 
@@ -47,9 +55,10 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+
 def sort_last(tuples):
-  # +++your code here+++
-  return
+    return sorted(tuples, key=lambda x: x[-1])
+  
 
 
 # Simple provided test() function used in main() to print
